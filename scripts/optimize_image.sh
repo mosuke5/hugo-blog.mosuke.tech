88b30b491c465a69717ff7e67881902c1c9ceba3
mosuke5 <w.vamos603@gmail.com>
2017-06-24 16:38:17 +0900

    optimize images

--- scripts/optimize_image.sh
+++ scripts/optimize_image.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-find ./static/image/ -name "*.png" | xargs optipng -o5
+find ./static/image/ -name "*.png" | xargs optipng -o7
 find ./static/image/ -name "*.jpg" -type f -exec jpegtran -copy none -optimize -outfile {} {} \;
