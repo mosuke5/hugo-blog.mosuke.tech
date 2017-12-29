03046e31168b1bac91dae161b671bea8ae959e8e
mosuke5 <w.vamos603@gmail.com>
2017-12-29 07:56:46 +0000

    modify bug of deleting cache

--- scripts/delete_top_page_cache.sh
+++ scripts/delete_top_page_cache.sh
@@ -3,4 +3,4 @@ curl -X DELETE "https://api.cloudflare.com/client/v4/zones/$1/purge_cache" \
      -H "X-Auth-Email: $2" \
      -H "X-Auth-Key: $3" \
      -H "Content-Type: application/json" \
-     --data '{"files": ["https://blog.mosuke.tech/index.html"]}'
+     --data '{"files": ["https://blog.mosuke.tech/"]}'
