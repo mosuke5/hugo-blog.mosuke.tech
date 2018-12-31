9fb10a7b71cd622999d435da4c85b7a898cbc8de
mosuke5 <w.vamos603@gmail.com>
2018-12-31 23:48:45 +0900

    add commited files to purge cache

--- scripts/delete_top_page_cache.sh
+++ scripts/delete_top_page_cache.sh
@@ -1,7 +1,37 @@
-#!/bin/sh
+#!/bin/bash
+# Params $1: cloud_flare zone id
+# Params $2: cloud_flare email
+# Params $3: cloud_flare api key
+# Params $4: git commit id
+
+# Add array to uris to purge contents uri
+uris=("/" "sitemap.xml" "index.xml")
+files=`git diff --name-only $4`
+for i in $files
+do
+    if [[ ${i} =~ ^(content/).*(.md) ]]; then
+        # "content/xxxx/aiueo.md" => "/xxxx/aiueo/"
+        uris+=("${i:7:-3}/")
+    elif [[ ${i} =~ ^(static/) ]]; then
+        # "static/image/aiueo.png" => "/image/aiueo.png"
+        uris+=("${i:6}")
+    fi
+done
+
+# Create arg param
+files_param=""
+base_url="https://blog.mosuke.tech"
+for uri in "${uris[@]}"
+do
+    echo "${base_url}${uri}"
+    files_param+="\"${base_url}${uri}\","
+done
+files_param="${files_param:0:-1}" #delete last comma
+
+# Exec CloudFlare API to purge cache
 sleep 10
 curl -X DELETE "https://api.cloudflare.com/client/v4/zones/$1/purge_cache" \
      -H "X-Auth-Email: $2" \
      -H "X-Auth-Key: $3" \
      -H "Content-Type: application/json" \
-     --data '{"files": ["https://blog.mosuke.tech/", "https://blog.mosuke.tech/sitemap.xml", "https://blog.mosuke.tech/index.xml"]}'
+     --data "{\"files\": [${files_param}]}"
