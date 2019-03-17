234729e3cc19b53f3fb8b8894de8372dc662350b
mosuke5 <w.vamos603+github@gmail.com>
2019-03-17 20:52:12 +0900

    Modify/optimize image script (#34)
    Update optimize_image.sh to improve optimizing speed

--- scripts/optimize_image.sh
+++ scripts/optimize_image.sh
@@ -1,3 +1,12 @@
-#!/bin/sh
-find ./static/image/ -name "*.png" | xargs optipng -o7
-find ./static/image/ -name "*.jpg" -type f -exec jpegtran -copy none -optimize -outfile {} {} \;
+#!/bin/bash
+
+set -e
+files=`git diff --name-only HEAD^`
+for i in $files
+do
+    if [[ ${i} =~ ^(static/).*(.png) ]]; then
+        optipng -o7 ${i}
+    elif [[ ${i} =~ ^(static/).*(.jpg|.jpeg) ]]; then
+        jpegtran -copy none -optimize -outfile ${i} ${i}
+    fi
+done
