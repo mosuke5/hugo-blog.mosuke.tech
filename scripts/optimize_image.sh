790447da71f3ca0a0ebc0eb15b382df1d27db8e4
mosuke5 <w.vamos603@gmail.com>
2019-03-17 19:48:25 +0900

    update

--- scripts/optimize_image.sh
+++ scripts/optimize_image.sh
@@ -1,3 +1,13 @@
-#!/bin/sh
-find ./static/image/ -name "*.png" | xargs optipng -o7
-find ./static/image/ -name "*.jpg" -type f -exec jpegtran -copy none -optimize -outfile {} {} \;
+#!/bin/bash
+#find ./static/image/ -name "*.png" | xargs optipng -o7
+#find ./static/image/ -name "*.jpg" -type f -exec jpegtran -copy none -optimize -outfile {} {} \;
+
+files=`git diff --name-only HEAD^`
+for i in $files
+do
+    if [[ ${i} =~ ^(static/).*(.png) ]]; then
+        optiping -o7 ${i}
+    elif [[ ${i} =~ ^(static/).*(.jpg|.jpeg) ]]; then
+        jpegtran -copy none -optimize -outfile ${i} ${i}
+    fi
+done
