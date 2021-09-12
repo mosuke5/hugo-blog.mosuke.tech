ca6f96f1a1347d0db03ff795622d687fdd9d4c42
mosuke5 <w.vamos603@gmail.com>
2021-09-12 19:15:24 +0900

    add error handling

--- scripts/delete_page_cache.sh
+++ scripts/delete_page_cache.sh
@@ -6,11 +6,15 @@
 # functions
 function purge_specific_cache () {
     sleep 10
-    curl -X DELETE "https://api.cloudflare.com/client/v4/zones/$1/purge_cache" \
+    result=`curl -X DELETE "https://api.cloudflare.com/client/v4/zones/$1/purge_cache" \
          -H "X-Auth-Email: $2" \
          -H "X-Auth-Key: $3" \
          -H "Content-Type: application/json" \
-         --data "{\"files\": [$4]}"
+         --data "{\"files\": [$4]}" | jq '.success'`
+
+    if [ $result = "false" ]; then
+      exit 1
+    fi
 }
 
 function purge_every_cache () {
